911edd23ff59d3c93f60370c8ca06c6389428f63
alvaroximenesrj <alvaro.junior.ext@talentfour.com.br>
2022-11-02 21:44:15 -0300

    return eniromnet

--- ASC.Solution/ASC.WebApi/Controllers/HomeController.cs
+++ ASC.Solution/ASC.WebApi/Controllers/HomeController.cs
@@ -1,23 +1,28 @@
+using ASC.WebApi.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace ASC.WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class HomeController : ControllerBase
-    {       
-
+    {
+        private IOptions<ApplicationSettings> _settings;  
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IOptions<ApplicationSettings> settings)
         {
             _logger = logger;
+            _settings = settings;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok("Hello World");
+            return Ok(_settings.Value.ApplicationTitle);
         }
     }
 }
